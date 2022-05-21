e1dda00aff8ffc4a5b087de03f3a366e4985d500
Your Name <you@example.com>
2022-05-22 01:10:59 +0600

    navbar and cover photo done

---
+++ src/Home/Banner/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='background-img flex items-center justify-center text-white font-bold font-serif'>
+            <div>
+                <h1 className='text-3xl'>We Believe In</h1>
+                <h2 className='text-2xl'>Best Quality &</h2>
+                <h2 className='text-2xl'>Best Support</h2>
+            </div>
+
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar';
+import Banner from './Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
