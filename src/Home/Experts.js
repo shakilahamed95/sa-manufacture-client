e5ea3d263680acd088a23b0b42435e806e122a82
Your Name <you@example.com>
2022-05-22 01:53:26 +0600

    one extra section done

---
+++ src/Home/Experts.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ceo from '../Images/ceo.jpg'
+import manager from '../Images/manager.jpg'
+import employee from '../Images/employee.jpg'
+
+
+const Experts = () => {
+    return (
+        <div>
+            <h1 className='text-primary text-center text-3xl font-bold my-6 '>Our Team Members</h1>
+            <div>
+                <div className='mx-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure><div class="avatar">
+                            <div class="w-48 rounded">
+                                <img src={ceo} alt='' />
+                            </div>
+                        </div></figure>
+                        <div class="card-body">
+                            <h2 class="text-2xl text-center text-primary">Eng Omar yeamin</h2>
+                            <h3 class="text-xl text-center text-primary"> CEO & Founder</h3>
+                        </div>
+                    </div>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure><div class="avatar">
+                            <div class="w-48 rounded">
+                                <img src={employee} alt='' />
+                            </div>
+                        </div></figure>
+                        <div class="card-body">
+                            <h2 class="text-2xl text-center text-primary">Eng Adam Meir</h2>
+                            <h3 class="text-xl text-center text-primary"> Production Manager</h3>
+                        </div>
+                    </div>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure><div class="avatar">
+                            <div class="w-48 rounded">
+                                <img src={manager} alt='' />
+                            </div>
+                        </div></figure>
+                        <div class="card-body">
+                            <h2 class="text-2xl text-center text-primary">Eng Jack Farnes</h2>
+                            <h3 class="text-xl text-center text-primary">Lead Mechanics</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Experts;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar';
 import Banner from './Banner/Banner';
+import Experts from './Experts';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Banner></Banner>
+            <Experts></Experts>
         </div>
     );
 };
